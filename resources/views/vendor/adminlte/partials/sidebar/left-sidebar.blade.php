162269b6fcfa37896f180ceee080d651173226bb
Jason RIos <161065488+JasonRios@users.noreply.github.com>
2024-03-07 11:51:16 -0600

    Revert "Cambio en el Sidebar"
    This reverts commit d0dd7daa04ee5f87d143d606cd92a5919b46a86a.

--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -9,42 +9,6 @@
 
     {{-- Sidebar menu --}}
     <div class="sidebar">
-    @auth
-     <!-- Autenticara que usuario ingreso y lo comparará en la condición para poner un mensaje -->
-     
-     <!-- Mensaje por usuarios -->
-
-        @if(Auth::user()->name == 'Admin')  <!-- Cambiar "Admin" por el nombre de usuario que tengan -->
-
-            <!-- Mostrar campo de texto solo para Alumnos -->
-            <P style="color: white;">¡Bienvenido, Admin!</p>        
-        @endif 
-        @if(Auth::user()->name == 'admin')
-            <!-- Mostrar campo de texto solo para Alumnos -->
-            <P style="color: white;">¡Bienvenido, admin!</p>
-        @endif 
-        @if(Auth::user()->name == 'jefe_division')
-            <!-- Mostrar campo de texto solo para Alumnos -->
-            <P style="color: white;">¡Bienvenido, Jefe de División!</p>
-        @endif 
-    @endauth
-     <!-- Mensaje por roles -->
-    @auth
-        @if(Auth::user()->hasRole('Alumno'))     <!-- Cambiar "Alumno" si es que tienen roles diferentes -->
-            <!-- Mostrar contenido para alumnos -->
-            <P style="color: white;">Estas accediendo como: Alumno</p>
-        @endif
-
-        @if(Auth::user()->hasRole('Admin'))
-            <!-- Mostrar contenido para alumnos -->
-            <P style="color: white;">Estas accediendo como: Admin</p>
-        @endif
-
-        @if(Auth::user()->hasRole('Personal'))
-            <!-- Mostrar contenido para alumnos -->
-            <P style="color: white;">Estas accediendo como: Personal</p>
-        @endif
-    @endauth
         <nav class="pt-2">
             <ul class="nav nav-pills nav-sidebar flex-column {{ config('adminlte.classes_sidebar_nav', '') }}"
                 data-widget="treeview" role="menu"
